bd9d0357cfc0c1d6501843f162f1bef850f1a4e1
james <jimmindungu3@gmail.com>
2025-07-02 09:54:59 +0300

    Initial commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        secondary: "#f8fafc",
+        tertiary: "#059669",
+        quaternary: "#64748b",
+      },
+    },
+  },
+  plugins: [],
+};
